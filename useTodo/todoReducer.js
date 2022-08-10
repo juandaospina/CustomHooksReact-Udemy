ef755aad499f23ea0a9e596b7f1041a2658c9373
Juan Ospina <juan.ospinal@outlook.com>
2022-08-10 18:01:33 -0500

    new folders

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,30 @@
+    
+export const todoReducer = ( state = [], action ) => {
+    switch ( action.type ) {
+        case '[TODO]: Add Todo':
+            return [
+                ...state,
+                action.payload
+            ]
+
+        case '[TODO]: Delete Todo':
+            return state.filter(
+                todo => todo.id !== action.payload
+            )
+
+        case '[TODO]: Completed Todo': 
+            return state.map( todo => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+    
+        default:
+            return state;
+    }
+}
--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../components/useReducer/todoReducer";
+import { todoReducer } from './todoReducer';
 
 
 const init = () => {
